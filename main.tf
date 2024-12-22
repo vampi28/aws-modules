d89c1e552c4f6a9f106a2470f2c7b2b1845f8d68
Diego Martinez <vampero18@gmail.com>
2024-12-22 18:33:25 +0100

    t

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
   secret_key                  = "test"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
 
   endpoints {
     s3 = "http://localhost:4566"
@@ -17,7 +28,7 @@ module "s3_bucket" {
   force_destroy = true
 
   tags = {
-    Environment = "Production"
+    Environment = "LocalStack"
     Project     = "Dynamic-Tags"
     Owner       = "Diego"
     Team        = "DevOps"
